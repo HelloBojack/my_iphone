868ec5893c524927cfc1e3ff64150bdf6d1b560f
HelloBojack <xlk970115@gmail.com>
2024-06-17 00:50:15 +0800

    feat: init header nav

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/assets/images/AppleImg.svg" alt="apple" />
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
