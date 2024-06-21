ea2ee3e671e3e997e47c0f8098f7a04deda47026
HelloBojack <xlk970115@gmail.com>
2024-06-21 22:16:38 +0800

    feat: navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,20 @@
+'use client'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
+  
+  
   return (
-    <div>Hero</div>
+    <section className='h-screen'>
+      <motion.div
+        className='font-semibold text-3xl text-gray-100 opacity-0 text-center'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        exit={{ opacity: 0 }}
+      >iPhone 15 Pro</motion.div>
+    </section>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,15 @@
 const NavBar = () => {
   return (
     <header>
-      <nav>
-        <img src="/assets/images/AppleImg.svg" alt="apple" />
+      <nav className="flex items-center justify-between p-[1rem]">
+        <img src="/assets/images/apple.svg" alt="apple" height={48} width={18} />
+        <div className="font-bold">
+          Apple
+        </div>
+        <div className="flex items-center justify-center">
+          <img src="/assets/images/bag.svg" alt="bag" height={48} width={18} className="mr-[1rem]"/>
+          <img src="/assets/images/search.svg" alt="search" height={48} width={18} />
+        </div>
       </nav>
     </header>
   )
