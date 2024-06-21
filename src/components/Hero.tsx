dd0079d4b042a9c5ef72797d17a1c31cd0adad76
HelloBojack <xlk970115@gmail.com>
2024-06-21 23:37:51 +0800

    feat: hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,13 +7,22 @@ const Hero = () => {
   
   return (
     <section className='h-screen'>
-      <motion.div
-        className='font-semibold text-3xl text-gray-100 opacity-0 text-center'
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1.5 }}
-        exit={{ opacity: 0 }}
-      >iPhone 15 Pro</motion.div>
+      <div className='h-5/6'>
+        <motion.p
+          className='font-semibold text-3xl text-gray-100 opacity-0 text-center'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5 }}
+          exit={{ opacity: 0 }}
+        >iPhone 15 Pro</motion.p>
+
+        <div className='h-[380px] overflow-hidden'>
+          <video autoPlay muted playsInline>
+            <source src="/assets/videos/hero.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      
     </section>
   )
 }
